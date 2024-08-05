28953d06e06dc25e3f74d419ea31d14c10b29bbd
- HARYs - <yousuf.ibnesalah@gmail.com>
2024-08-05 20:24:32 +0500

    support.js

--- src/commands/bot/support.js
+++ src/commands/bot/support.js
@@ -5,15 +5,15 @@ module.exports = async (client, interaction, args) => {
         .addComponents(
             new Discord.ButtonBuilder()
                 .setLabel("Support server")
-                .setURL(https://discord.gg/xCZJmsZFBv)
+                .setURL(client.config.discord.serverInvite)
                 .setStyle(Discord.ButtonStyle.Link),
         );
 
     client.embed({
         title: `❓・Support`,
-        desc: `Developed by - HARYs -`,
+        desc: `Make your server even better with Bot!`,
         image: "https://cdn.discordapp.com/attachments/843487478881976381/874694194474668052/Bot_banner_invite.jpg",
-        url: client.config.https://discord.gg/xCZJmsZFBv,
+        url: client.config.discord.serverInvite,
         components: [row],
         type: 'editreply'
     }, interaction)
