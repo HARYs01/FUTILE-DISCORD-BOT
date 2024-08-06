121e1044fb04346a28cddf4070bb61fb8a58fe5a
- HARYs - <yousuf.ibnesalah@gmail.com>
2024-08-06 15:45:41 +0500

    donate.js

--- src/commands/bot/donate.js
+++ src/commands/bot/donate.js
@@ -4,8 +4,8 @@ module.exports = async (client, interaction, args) => {
     let row = new Discord.ActionRowBuilder()
         .addComponents(
             new Discord.ButtonBuilder()
-                .setLabel("CorwinDev GitHub")
-                .setURL("https://github.com/sponsors/CorwinDev")
+                .setLabel("It's just for fun")
+                .setURL("https://discord.com/oauth2/authorize?client_id=1270289292152934439&permissions=2147483648&integration_type=0&scope=applications.commands+bot")
                 .setStyle(Discord.ButtonStyle.Link),
         );
 
@@ -13,7 +13,7 @@ module.exports = async (client, interaction, args) => {
         title: `${client.user.username}・Donate`,
         desc: '_____ \n\nClick the button below for the sponsor page \n**Pay attention! sponsor is not required**',
         thumbnail: client.user.avatarURL({ dynamic: true }),
-        url: "https://github.com/sponsors/CorwinDev",
+        url: "https://discord.com/oauth2/authorize?client_id=1270289292152934439&permissions=2147483648&integration_type=0&scope=applications.commands+bot",
         components: [row],
         type: 'editreply'
     }, interaction)
